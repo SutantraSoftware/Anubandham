1f7408a161e54348dc50403076834fd8c708f1e2
SutantraSoftware <150811906+SutantraSoftware@users.noreply.github.com>
2023-11-17 18:14:08 +0530

    religion name correction
    religion name correction

--- controllers/religionController.js
+++ controllers/religionController.js
@@ -2,10 +2,10 @@ const Religion = require('../models/Religion')
 
 const createReligion = async(req,res)=>{
     try{
-        const {regionName,regionValue} = req.body
+        const {religionName,religionValue} = req.body
 
         const addreligion = new Religion({
-            regionName,regionValue
+            religionName,religionValue
         })
         await addreligion.save()
         res.status(201).json(addreligion)
